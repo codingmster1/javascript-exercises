ba9d6064a29e76ae52c4308cc7168eac50001b5f
Jeff <doylejeff98@gmail.com>
2022-10-07 14:21:38 -0400

    commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -5,7 +5,7 @@ let values = [1, 2, 4];
 removeFromArray = removeFromArray.filter(item => !values.includes(item));
 console.log(removeFromArray);
 
-};
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
