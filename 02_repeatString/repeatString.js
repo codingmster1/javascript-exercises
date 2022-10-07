905b12992b6a64985166251eba4b4e7e98452c24
Jeff <doylejeff98@gmail.com>
2022-10-07 13:49:21 -0400

    commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -5,20 +5,20 @@ const repeatString = function(string, times)
     return string.repeat(times);
     times = 3;
     
-}
+
 
 repeatString("hey", 3);
 
-let repeatString1 = function(string, times)
+var repeatString1 = function(string, times)
 {
     if (times > 0)
     return string.repeat(times);
     times = 10;
 }
 
-repeatString1("hey", 0);
+repeatString1("hey", 10);
 
-
+}
 // Do not edit below this line
 module.exports = repeatString;
 
