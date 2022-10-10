6a48286c72d7fb155a40b0ba99ba562a377b8d21
Jeff <doylejeff98@gmail.com>
2022-10-10 14:09:01 -0400

    commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,12 @@
-const removeFromArray = function(arr, three) {
-var arr = [1, 2, 3, 4];
-var three = arr.splice(3);
+const removeFromArray = function(arr) {
 
+    var badValues = Array.prototype.slice.call(arguments);
+    badValues.shift(); 
+    return arr.filter(function(x) {return badValues.indexOf(x) == -1;});
 
 }
+removeFromArray([1, 2, 3, 4], 3);
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
