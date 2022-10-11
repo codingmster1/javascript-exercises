4519dcc82eda0c94302a0356798d06b3c7daf58f
Jeff <doylejeff98@gmail.com>
2022-10-11 13:05:29 -0400

    commit

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, d, range=[]) {
 
-    
-};
+    if (a < 0 || b < 0) return 'ERROR'
+    if (isNaN(a) || isNaN(b)) return 'ERROR'
+
+    for (let i = Math.min(a, d); i<= Math.max(a, d); i++) {
+        range.push(i)
+    }
+
+    return range.reduce((a, d) => a + b) 
+}
+
+console.log(sumAll(1,10));
+console.log(sumAll(10,1));
+console.log(sumAll(-10,1));
+console.log(sumAll(1,-10));
+console.log(sumAll(1,'a'));
+console.log(sumAll('a',1));
+console.log(sumAll('a',-1));
 
 
 
