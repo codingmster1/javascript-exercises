b0c76a939693f5c41a5e330eae943e63c037323a
Jeff <doylejeff98@gmail.com>
2022-10-12 14:28:04 -0400

    commit

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,11 +1,11 @@
 const leapYears = function(year) {
 return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 ===0);
 }
-console.log(leapYear(2016));
-console.log(leapYear(2000));
-console.log(leapYear(1700));
-console.log(leapYear(1800));
-console.log(leapYear(100));
+console.log(leapYears(2016));
+console.log(leapYears(2000));
+console.log(leapYears(1700));
+console.log(leapYears(1800));
+console.log(leapYears(100));
 
 // Do not edit below this line
 module.exports = leapYears;
